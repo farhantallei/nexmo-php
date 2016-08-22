6be248348d35b24bbae01c15e6bb147be55e293c
farhantallei <farhan.pradana@ymail.com>
2016-08-23 04:16:51 +0700

    Update send.php

--- examples/send.php
+++ examples/send.php
@@ -3,7 +3,7 @@
 require_once '../vendor/autoload.php';
 
 //create client with api key and secret
-$client = new Nexmo\Client(new Nexmo\Client\Credentials\Basic(API_KEY, API_SECRET));
+$client = new Nexmo\Client(new Nexmo\Client\Credentials\Basic(ac6cc35d, db3143a6e824b274));
 
 //send message using simple api params
 $message = $client->message()->send([
